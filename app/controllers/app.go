06e61b37f6b094f22321f60042c17fa37360b981
rene <renegmed@yahoo.com>
2016-06-24 08:30:36 -0400

    1-Tour init commit

---
+++ app/controllers/app.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/revel/revel"
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Order() revel.Result {
+	return c.Render()
+}
+
+func (c App) CreateAccount() revel.Result {
+	return c.RenderTemplate("App/create_account.html")
+}
+
+func (c App) Payment() revel.Result {
+	return c.Render()
+}
